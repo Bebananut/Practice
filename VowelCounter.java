9bc25176a98dd075903054d320a67079ba4807aa
Bebananut <test.forguii@gmail.com>
2022-10-29 20:59:05 +0700

    change method name, refactor method countVowel

--- VowelCounter.java
+++ VowelCounter.java
@@ -4,19 +4,28 @@ class VowelCounter{
         Scanner sc = new Scanner(System.in);
         System.out.print("Input :");
         String input = sc.nextLine();
-        System.out.println("Total vowel :"+count(input));
+        System.out.println("Total vowel :"+countVowel(input));
     }
 
-    public static int count(String input){
+    public static int countVowel(String input){
         int count = 0;
         input = input.toLowerCase();
         for(int i = 0 ; i<input.length() ; i++ ){
             String splitCharactor = input.substring(i, i+1);
-            if(splitCharactor.equals("a")||splitCharactor.equals("e")||splitCharactor.equals("i")
-                ||splitCharactor.equals("o")||splitCharactor.equals("u")){
+            if(isVowel(splitCharactor)){
                 count++;
             }
         }
         return count;
     }
+
+    public static boolean isVowel(String splitCharactor){
+        if(splitCharactor.equals("a")||splitCharactor.equals("e")||splitCharactor.equals("i")
+            ||splitCharactor.equals("o")||splitCharactor.equals("u")){
+            return true;        
+        }else{
+            return false;
+        }
+    }
+    
 }
