80246b354dd152743dace1dd995ecb13b5925c3b
Bebananut <test.forguii@gmail.com>
2022-10-30 20:54:39 +0700

    change name varaible

--- VowelCounter.java
+++ VowelCounter.java
@@ -1,9 +1,9 @@
 import java.util.*;
 class VowelCounter{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner inputReceiver = new Scanner(System.in);
         System.out.print("Input :");
-        String input = sc.nextLine();
+        String input = inputReceiver.nextLine();
         System.out.println("Total vowel :"+countVowel(input));
     }
 
