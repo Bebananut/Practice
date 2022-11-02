6de08da76abc031529588d5cbe8cb5887c52f1cb
Bebananut <test.forguii@gmail.com>
2022-11-02 21:17:33 +0700

    change style name, separte logic compare vowels

--- VowelCounter.java
+++ VowelCounter.java
@@ -10,9 +10,14 @@ class VowelCounter{
     }
 
     public static int countVowel(String input){
-        List<Character> vowels = Arrays.asList('a','e','i','o','u');
         List<Character> characters  = input.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
-        return (int) characters.stream().filter(character->vowels.contains(Character.toLowerCase(character))).count();
+        return (int) characters.stream().filter(character->isVowel(character)).count();
+    }
+
+    public static boolean isVowel(char character){
+        char characterLowerCase = Character.toLowerCase(character);
+        List<Character> vowels = Arrays.asList('a','e','i','o','u');
+        return vowels.contains(characterLowerCase);
     }
 
 }
