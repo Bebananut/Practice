98c6c205341b0a7fcda7f1324d8876ef18d29926
Bebananut <test.forguii@gmail.com>
2022-11-02 21:51:25 +0700

    add spacing

--- VowelCounter.java
+++ VowelCounter.java
@@ -9,12 +9,12 @@ class VowelCounter{
         System.out.println("Total vowel :"+countVowel(input));
     }
 
-    public static int countVowel(String input){
+    public static int countVowel(String input) {
         List<Character> characters  = input.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
         return (int) characters.stream().filter(character -> isVowel(character)).count();
     }
 
-    public static boolean isVowel(char character){
+    public static boolean isVowel(char character) { 
         char characterLowerCase = Character.toLowerCase(character);
         List<Character> vowels = Arrays.asList('a','e','i','o','u');
         return vowels.contains(characterLowerCase);
